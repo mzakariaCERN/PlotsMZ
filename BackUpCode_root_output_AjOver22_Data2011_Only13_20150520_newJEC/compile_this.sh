f0186fefd2b1742256a85a68148ceca972b21113
mzakariaCERN <mzakaria@uic.edu>
2015-06-08 08:15:42 -0500

    adding root output for Aj > 0.22 NewJEC

---
+++ BackUpCode_root_output_AjOver22_Data2011_Only13_20150520_newJEC/compile_this.sh
@@ -0,0 +1,5 @@
+#g++ -Wall -I$ROOTSYS/include -L$ROOTSYS/lib `root-config --libs --glibs` mixing_tree.C HT_Analyzer4.C -o HT_Analyzer4.out
+g++ -Wall -I$ROOTSYS/include -L$ROOTSYS/lib `root-config --libs --glibs` mixing_tree.C HT_Analyzer5.C -o HT_Analyzer5.out
+
+
+
