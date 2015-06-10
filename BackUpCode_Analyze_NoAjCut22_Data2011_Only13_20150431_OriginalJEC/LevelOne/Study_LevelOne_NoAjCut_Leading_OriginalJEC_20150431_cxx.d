04441aac7e5c2d6c0cca3bacd9c4111b9fac0b86
mzakariaCERN <mzakaria@uic.edu>
2015-06-09 20:06:30 -0500

    cleaning 20160609

--- BackUpCode_Analyze_NoAjCut22_Data2011_Only13_20150431_OriginalJEC/LevelOne/Study_LevelOne_NoAjCut_Leading_OriginalJEC_20150431_cxx.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./Study_LevelOne_NoAjCut_Leading_OriginalJEC_20150431_cxx.so: /home/mzakaria3/Documents/Research/scratch/v5-34-00-patches/include/cintdictversion.h /home/mzakaria3/Documents/Research/scratch/v5-34-00-patches/include/RVersion.h
-Study_LevelOne_NoAjCut_Leading_OriginalJEC_20150431_cxx__ROOTBUILDVERSION= 5.34/25
--- BackUpCode_Analyze_NoAjCut22_Data2011_Only13_20150431_OriginalJEC/LevelTwo/Study_LevelTwo_AjOver22_Leading20150520_cxx.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./Study_LevelTwo_AjOver22_Leading20150520_cxx.so: /home/mzakaria3/Documents/Research/scratch/v5-34-00-patches/include/cintdictversion.h /home/mzakaria3/Documents/Research/scratch/v5-34-00-patches/include/RVersion.h
-Study_LevelTwo_AjOver22_Leading20150520_cxx__ROOTBUILDVERSION= 5.34/25
